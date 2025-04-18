586aabd6abe713bfcb1dff5c2aebbb0e846b4221
anuksalgado <118955816+anuksalgado@users.noreply.github.com>
2025-04-18 19:57:29 +0800

    Removed IOS, Windows and MAC OBJ files and BIN files

--- DSMAUI/obj/Debug/net9.0-android/DSMAUI.AssemblyInfo.cs
+++ DSMAUI/obj/Debug/net9.0-android/DSMAUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DSMAUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+362a715c8d62229d42cfdb2358de9dec49f9efca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+3c666b117299df3bea8043ccf25f7f9d4a553f6c")]
 [assembly: System.Reflection.AssemblyProductAttribute("DSMAUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DSMAUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
